f2917c6e37c4c699adb5fcabd7130170b13e8df7
LuisDam1B <luis.narvaez@iesdoctorbalmis.com>
2020-01-15 12:03:12 +0100

    Agregar archivos de proyecto.

---
+++ AplicacionMVVC/MainWindowVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionMVVC
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+
+        tema6bdluisEntities contexto = new tema6bdluisEntities();
+
+        public MainWindowVM()
+        {
+            contexto.CLIENTES.Load();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
